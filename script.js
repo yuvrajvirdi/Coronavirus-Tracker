79773181accd60890ee9328594450d0e739e0c62
yuvrajvirdi <81879713+yuvrajvirdi@users.noreply.github.com>
2021-08-19 00:52:08 -0400

    added more data using the api

--- script.js
+++ script.js
@@ -9,11 +9,26 @@ function displayData(){
     .then((data) => {
         console.log(data);
         document.getElementById("name").innerHTML = data.country;
+
+        document.getElementById("tests").innerHTML = data.tests;
         document.getElementById("cases").innerHTML = data.cases;
         document.getElementById("active-cases").innerHTML = data.active;
         document.getElementById("critical-cases").innerHTML = data.critical;
         document.getElementById("recovered").innerHTML = data.recovered;
         document.getElementById("deaths").innerHTML = data.deaths;
-        document.getElementById("tests").innerHTML = data.tests;
+        document.getElementById("population").innerHTML = data.population;
+
+        document.getElementById("confirmed-percents").innerHTML = ((data.cases/data.population)*100).toFixed(2)+"%";
+        document.getElementById("active-percents").innerHTML = ((data.active/data.population)*100).toFixed(2)+"%";
+        document.getElementById("critical-percents").innerHTML = ((data.critical/data.population)*100).toFixed(4)+"%";
+        document.getElementById("recovered-percents").innerHTML = ((data.recovered/data.cases)*100).toFixed(2)+"%";
+        document.getElementById("deaths-percents").innerHTML = ((data.deaths/data.cases)*100).toFixed(2)+"%";
+
+        document.getElementById("tests-stats").innerHTML=Math.round(data.testsPerOneMillion);
+        document.getElementById("confirmed-stats").innerHTML = Math.round(data.casesPerOneMillion);
+        document.getElementById("active-stats").innerHTML = Math.round(data.activePerOneMillion);
+        document.getElementById("critical-stats").innerHTML = Math.round(data.criticalPerOneMillion);
+        document.getElementById("recovered-stats").innerHTML = Math.round(data.recoveredPerOneMillion);
+        document.getElementById("deaths-stats").innerHTML = Math.round(data.deathsPerOneMillion);
     });
 }
